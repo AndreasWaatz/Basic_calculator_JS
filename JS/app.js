319f6906d66766981de376cb8ff3acecd1c3277c
AndreasWaatz <andreaswatz@gmail.com>
2019-09-13 23:15:06 +0200

    Adding more functionality with . and able to calculate more numbers then 2

--- JS/app.js
+++ JS/app.js
@@ -1,80 +1,105 @@
-/* Ideas 87
-1. Add comma to create 4,6 * 5;
-2. Kunna räna fler än 2 tal
-*/
+//Basic calculator created by Andreas Wåtz
 
+// Listen to button clicked
 let numbers = document.querySelector('.numberButtons');
 numbers.addEventListener('click', btnClicked);
 
+// Get's the calculations field
 let calculationField = document.querySelector('#calculations');
 
+//Stores numbers clicked
 let numberClicked = [];
-let numberClickedSecond = [];
+// Store Joined numbers from numbersClicked
 let joinedNumbers = [];
-let joinedNumbersSecond = [];
-let operator = '';
-let active = true;
+// Store all inputs woth numbers and operator
+let calculationsArray = [];
+// Adds all the numbers depending on operator
+let totalResult = 0;
 
+// Starts function when buttons clicked
 function btnClicked(e) {
 
+    // Stores value of the button clicked
     let btnClicked = e.target.value;
 
+    // If C is clicked everythig gets cleared
     if(btnClicked === 'c') {
         calculationField.innerText = '0';
+        totalResult = 0;
+        calculationsArray = [];
         numberClicked = [];
         numberClickedSecond = [];
         active = true;
-    }
-    
-    if(active === true) {
+     //If a number is clicked we print it out and stores the values in array and join the to one.   
+    }else if(btnClicked === '0' || btnClicked === '1' || btnClicked === '2' || btnClicked === '3' ||    btnClicked === '4' || btnClicked === '5' || btnClicked === '6' || btnClicked === '7' ||         btnClicked === '8' || btnClicked === '9' || btnClicked === '.' ) {
 
-        if(btnClicked === '0' || btnClicked === '1' || btnClicked === '2' || btnClicked === '3' || btnClicked === '4' || btnClicked === '5' || btnClicked === '6' || btnClicked === '7' || btnClicked === '8' || btnClicked === '9' ) {
+
+        // Checks if desimal is clicked
+        if(btnClicked === '.'){
+            //Push decimal to numberClicked
             numberClicked.push(btnClicked);
+            //Prints out number clicked + decimal
+            calculationField.innerText = joinedNumbers + '.';
+        }else{
+            //Push each number to numberClicked
+            numberClicked.push(btnClicked);
+            //Join numbers to one integer
             joinedNumbers = +numberClicked.join('');
+            //Prints out numbers clicked
             calculationField.innerText = joinedNumbers;
+        }
 
-        } else if(btnClicked === "+" || btnClicked === "-" || btnClicked === "x" || btnClicked === "/"){
-
-            operator = btnClicked;
-
+      // Adds latest number and clicked operators to calculationArray      
+    } else if(btnClicked === "+" || btnClicked === "-" || btnClicked === "x" || btnClicked === "/"){
+            //Clears numbersClicked
+            numberClicked = [];
+            //Push joinNumbers to CalculateArray
+            calculationsArray.push(joinedNumbers);
+            //Clears joinedNumbers
+            joinedNumbers = [];
+            //Adds operator to calculationsArray
+            calculationsArray.push(btnClicked);
+            //Print out the operator to field
             calculationField.innerText = btnClicked;
     
-            active = false;
-        }    
-        
-    } else if(active === false) {
+      // Goes thru the calculationsArray and calculate all the number depending on operator in array      
+    } else if(btnClicked === '=') {
+            //Adds last number input to calculationsArray
+            calculationsArray.push(joinedNumbers);
+            //Clears numberClicked
+            numberClicked = [];
+            //Adds first number value in calculationArray to totalResult
+            totalResult = calculationsArray[0];
 
-        if(btnClicked === '0' || btnClicked === '1' || btnClicked === '2' || btnClicked === '3' || btnClicked === '4' || btnClicked === '5' || btnClicked === '6' || btnClicked === '7' || btnClicked === '8' || btnClicked === '9') {
+            //Loops thru numbers and operators and store it in totalResult.
+            for (let i = 0; i < calculationsArray.length; i++) {
 
-            numberClickedSecond.push(btnClicked);
-            joinedNumbersSecond = +numberClickedSecond.join('');
-            calculationField.innerText = joinedNumbersSecond;
-
-        } else if(btnClicked === '=') {
-
-            if(operator === '+'){
-                calculationField.innerText = joinedNumbers + joinedNumbersSecond;
-            } else if(operator === 'x') {
-                calculationField.innerText = joinedNumbers * joinedNumbersSecond;
-            } else if(operator === '-') {
-                calculationField.innerText = joinedNumbers - joinedNumbersSecond;
-            } else if(operator === '/') {
-                calculationField.innerText = joinedNumbers / joinedNumbersSecond;
+                if(calculationsArray[i] === '+'){
+                    console.log('Operator',calculationsArray[i]);
+                    totalResult += calculationsArray[i +1]
+                } else if(calculationsArray[i] === '-'){
+                    console.log('Operator',calculationsArray[i]);
+                    totalResult -= calculationsArray[i +1]
+                } else if(calculationsArray[i] === 'x'){
+                    console.log('Operator',calculationsArray[i]);
+                    totalResult *= calculationsArray[i +1]
+                } else if(calculationsArray[i] === '/'){
+                    console.log('Operator',calculationsArray[i]);
+                    totalResult /= calculationsArray[i +1]
+                }    
+                    
             }
-        }
-    }
+            // Check if totalResult is number without decimal
+            if (totalResult == Math.floor(totalResult)) {
+                //Prints out result without decimal.
+                calculationField.innerText = totalResult
+            }else {
+                //Print out result in field with maximum 2 numbers after decimal
+            calculationField.innerText = totalResult.toFixed(2);
+            }
+        } 
 }
 
-let calcOperators = (function(){
 
-    function calcAddition(value1, value2) {
-            return value1 + value2;
-    }
-
-    return {
-        calcAddition: calcAddition
-    }
-
-})();
 
 
